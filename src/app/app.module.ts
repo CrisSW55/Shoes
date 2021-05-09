2743bdbe929d1ed41b9bcb99b8ccc6bdb608b727
CrisSW55 <crystianguzman55@gmail.com>
2021-05-09 02:07:43 -0700

    added angular website project to firebase hosting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,23 @@ import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './components/home/home.component';
 import { InventoryComponent } from './components/inventory/inventory.component';
 
+// 1. Import the libs you need
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+
+const config = {
+  apiKey: "AIzaSyALavRPl7_qsB8P5YGhuoU3WQKX4ppNFwo",
+  authDomain: "clothing-447ee.firebaseapp.com",
+  projectId: "clothing-447ee",
+  storageBucket: "clothing-447ee.appspot.com",
+  messagingSenderId: "389395209541",
+  appId: "1:389395209541:web:c9d5cd5bca31f60262322c",
+  measurementId: "G-SZWV1DT25S"
+};
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +40,12 @@ import { InventoryComponent } from './components/inventory/inventory.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+     // 3. Initialize
+     AngularFireModule.initializeApp(config),
+     AngularFirestoreModule, // firestore
+     AngularFireAuthModule, // auth
+     AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
